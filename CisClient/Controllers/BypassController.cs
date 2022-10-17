8e49fabd6d5f05cb82a888d33ac55cb99a09ddea
fstrateg <Alexeym78@mail.ru>
2022-10-17 17:55:53 +0600

    Net core Mvc

---
+++ CisClient/Controllers/BypassController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CisClient.Controllers
+{
+    [Route("bypass")]
+    [ApiController]
+    public class BypassController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
